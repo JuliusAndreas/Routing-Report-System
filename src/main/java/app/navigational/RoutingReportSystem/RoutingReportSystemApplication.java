ecbc0de85a81bbbb5069fdfeb5ea8ad42c533b02
Soroosh Faal <faalsoroosh@gmail.com>
2023-09-13 10:53:44 +0330

    create the project

---
+++ src/main/java/app/navigational/RoutingReportSystem/RoutingReportSystemApplication.java
@@ -0,0 +1,13 @@
+package app.navigational.RoutingReportSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingReportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingReportSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/navigational/RoutingReportSystem/RoutingReportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package app.navigational.RoutingReportSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingReportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
