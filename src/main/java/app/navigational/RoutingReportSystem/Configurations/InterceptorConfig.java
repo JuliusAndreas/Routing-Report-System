11f96ec1f59be44950f4e9d795adb16779a3864b
Soroosh Faal <faalsoroosh@gmail.com>
2023-09-18 20:00:10 +0330

    add necessary security, filter & interceptor configs

---
+++ src/main/java/app/navigational/RoutingReportSystem/Configurations/InterceptorConfig.java
@@ -0,0 +1,18 @@
+package app.navigational.RoutingReportSystem.Configurations;
+
+import app.navigational.RoutingReportSystem.Interceptors.TokenUsernameExtractorInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Autowired
+    TokenUsernameExtractorInterceptor extractorInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(extractorInterceptor);
+    }
+}
